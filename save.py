f45fa5426ff5ed77934afc8ac562c5a291726d2d
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-10-13 01:50:42 +0300

    renamed save > compress & added logging

--- save.py
+++ save.py
@@ -1,11 +1,26 @@
 from PIL import Image
 import io, os
+from pathlib import Path
 from exceptions import SaveError
+from log_config import logger
 
-def save(input_path, output_path, min_resolution=600, max_resolution=1200, max_file_size=240):
+def compress(input_image, output_path, output_filename = None, min_resolution=600, max_resolution=1200, max_file_size=240):
     try:
+        logger.trace(f"Saving image to {output_path}...")
+        input_filename = Path(input_image).name.split(".")[0]
+        input_extension = Path(input_image).name.split(".")[1]
+        filename = output_filename or f"{input_filename}_compressed.{input_extension}"
+        output_path = os.path.join(output_path, filename)
+
         # Open the input image
-        img = Image.open(input_path)
+        if isinstance(input_image, str) or isinstance(input_image, bytes) or isinstance(input_image, Path):
+            img = Image.open(input_image)
+        elif isinstance(input_image, Image.Image):
+            img = input_image
+        elif isinstance(input_image, list):
+            img = Image.fromarray(input_image, "RGB")
+        else:
+            raise SaveError(f"Invalid input image type: {type(input_image)}")
 
         # Get the original image dimensions
         width, height = img.size
@@ -13,8 +28,9 @@ def save(input_path, output_path, min_resolution=600, max_resolution=1200, max_f
         # Check if the image is already within the desired resolution and aspect ratio
         if width == height and min_resolution <= width <= max_resolution:
             # No need to compress, just save the original image
+            logger.info("Image already within desired resolution and aspect ratio. No need to compress. Saving original image...")
             img.save(output_path)
-            return
+            return True
 
         # Calculate the new dimensions while maintaining the aspect ratio
         new_width, new_height = width, height
@@ -24,6 +40,7 @@ def save(input_path, output_path, min_resolution=600, max_resolution=1200, max_f
 
         # Resize the image to the new dimensions
         img = img.resize((new_width, new_height), Image.LANCZOS)
+        logger.trace(f"Image resized to {new_width}x{new_height}")
 
         # Save the compressed image with varying quality
         quality = 95
@@ -35,9 +52,15 @@ def save(input_path, output_path, min_resolution=600, max_resolution=1200, max_f
                 break
             quality -= 5
 
+        filename = output_filename or f"{input_filename}_compressed_{quality}.{input_extension}"
+        output_path = os.path.join(output_path, filename)
+
         # Save the final compressed image
         img.save(output_path, format="JPEG", quality=quality)
+        logger.success(f"Image saved with quality {quality} and size {buffer_size / 1024} KB to {output_path}")
+        return True
 
     except Exception as e:
-        raise SaveError(f"Error: {e}")
+        raise SaveError(e)
+
 
