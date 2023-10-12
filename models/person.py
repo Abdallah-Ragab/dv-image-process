a56011652c26998103b7493d357d6d340526be5e
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-10-13 01:47:48 +0300

    each model passage assessment into separate function

--- models/person.py
+++ models/person.py
@@ -3,59 +3,63 @@ from .face import Face
 from .body import Body
 from .head import Head
 
+
 class Person(Face, Body, Head):
     INFO = OBJ()
     RESULTS = OBJ()
+
     def __init__(self, image):
         self.image = image
         self.dimensions = OBJ(x=image.shape[1], y=image.shape[0])
-        self.face = self.get_face_landmarks()
-        super().__init__()
         self.gather_info()
         self.determine_results()
+
     def gather_info(self):
         for base in __class__.__bases__:
             if hasattr(base, "gather_info"):
                 base.gather_info(self)
 
     def determine_results(self):
-        face_detected = bool(self.INFO.face.rotation)
-        if face_detected and self.INFO.face.rotation.angles:
-            face_rotation = (self.INFO.face.rotation.angles.x, self.INFO.face.rotation.angles.y, self.INFO.face.rotation.angles.z)
-            glasses_detected = bool(self.INFO.face.glasses)
+        face_passed = self.face_passed()
+        glasses_passed = self.glasses_passed()
+        shoulders_passed = self.shoulders_passed()
 
-        shoulders_detected = bool(self.INFO.shoulders.slope)
-        if shoulders_detected:
-            shoulders_slope = self.INFO.shoulders.slope
-
-        face_passed = (
-            face_detected and
-            abs(float(face_rotation[0])) < 2 and
-            abs(float(face_rotation[1])) < 2 and
-            abs(float(face_rotation[2])) < 2
-        ) if face_detected else False
-
-        glasses_passed = face_detected and not glasses_detected
-        shoulders_passed = shoulders_detected and abs(float(shoulders_slope)) < 3
-        passed = (face_passed and glasses_passed and shoulders_passed) if shoulders_detected else (face_passed and glasses_passed)
-
-        face_results = OBJ(
-            detected = face_detected,
-            passed = face_passed
-        )
-        glasses_results = OBJ(
-            detected = face_detected and glasses_detected,
-            passed = glasses_passed
-        )
-        shoulders_results = OBJ(
-            detected = shoulders_detected,
-            passed = shoulders_passed
-        )
+        if self.INFO.shoulders.success:
+            passed = (face_passed and glasses_passed and shoulders_passed)
+        else:
+            passed = (face_passed and glasses_passed)
 
         self.RESULTS = OBJ(
-            face = face_results,
-            glasses = glasses_results,
-            shoulders = shoulders_results,
-            passed = passed
+            passed = passed,
+            face = OBJ(passed = face_passed, detected = self.INFO.face.success),
+            glasses = OBJ(passed = glasses_passed, detected = self.INFO.face.glasses.success),
+            shoulders = OBJ(passed = shoulders_passed, detected = self.INFO.shoulders.success)
         )
 
+    def face_passed(self):
+        if self.INFO.face.success and self.INFO.face.rotation.success:
+            return (
+                self.INFO.face.success
+                and abs(float(self.INFO.face.rotation.angles.x)) < 2
+                and abs(float(self.INFO.face.rotation.angles.y)) < 2
+                and abs(float(self.INFO.face.rotation.angles.z)) < 2
+            )
+        else:
+            return False
+
+    def glasses_passed(self):
+        if self.INFO.face.success and self.INFO.face.glasses.success:
+            return (
+                self.INFO.face.success
+                and self.INFO.face.glasses.success
+                and not self.INFO.face.glasses.detected
+            )
+        else:
+            return False
+
+    def shoulders_passed(self):
+        if self.INFO.shoulders.success:
+            shoulders_slope = self.INFO.shoulders.slope or 999
+            return abs(float(shoulders_slope)) < 3
+        else:
+            return False
